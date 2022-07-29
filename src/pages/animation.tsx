1acfe207aae964f60b870d37704c2c6ffe21880c
Alejandro Oviedo <alexoviedo999@gmail.com>
2022-07-28 22:53:14 -0500

    feat: add react three fiber, remove vanta, add daisy ui and refactor UI

---
+++ src/pages/animation.tsx
@@ -0,0 +1,85 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useMemo, useRef } from 'react';
+import * as THREE from 'three';
+
+const roundedSquareWave = (t: number, delta: number, a: number, f: number) => {
+  return ((2 * a) / Math.PI) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta);
+};
+
+function Dots() {
+  const ref = useRef<{ setMatrixAt: Function; instanceMatrix: any }>(null);
+  const { vec, transform, positions, distances } = useMemo(() => {
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const vec = new THREE.Vector3();
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const transform = new THREE.Matrix4();
+
+    // Precompute randomized initial positions
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const positions = [...Array(10000)].map((_, i) => {
+      const position = new THREE.Vector3();
+      // Place in a grid
+      position.x = (i % 100) - 50;
+      position.y = Math.floor(i / 10) - 50;
+
+      // Offset every other column (hexagonal pattern)
+      position.y += (i % 2) * 0.5;
+
+      // Add some noise
+      position.x += Math.random() * 0.3;
+      position.y += Math.random() * 0.3;
+      return position;
+    });
+
+    // Precompute initial distances with octagonal offset
+    const right = new THREE.Vector3(1, 0, 0);
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const distances = positions.map((pos) => {
+      return pos.length() + Math.cos(pos.angleTo(right) * 8) * 0.5;
+    });
+    return { vec, transform, positions, distances };
+  }, []);
+  useFrame(({ clock }) => {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < 10000; i++) {
+      const dist = distances[i];
+
+      // Distance affects the wave phase
+      const t = clock.elapsedTime - dist / 25;
+
+      // Oscillates between -0.4 and +0.4
+      const wave = roundedSquareWave(t, 0.15 + (0.2 * dist) / 72, 0.4, 1 / 3.8);
+
+      // Scale initial position by our oscillator
+      vec.copy(positions[i]).multiplyScalar(wave + 1.3);
+
+      // Apply the Vector3 to a Matrix4
+      transform.setPosition(vec);
+
+      // Update Matrix4 for this instance
+      ref.current?.setMatrixAt(i, transform);
+    }
+    if (ref.current) {
+      ref.current.instanceMatrix.needsUpdate = true;
+    }
+  });
+  return (
+    <instancedMesh ref={ref} args={[null, null, 10000]}>
+      <circleBufferGeometry args={[0.15]} />
+      <meshBasicMaterial />
+    </instancedMesh>
+  );
+}
+
+export default function Animaton() {
+  return (
+    <div className="scene">
+      <Canvas orthographic camera={{ zoom: 20 }}>
+        <color attach="background" args={['black']} />
+        <Dots />
+      </Canvas>
+    </div>
+  );
+}
--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -13,49 +13,142 @@ const Main = (props: IMainProps) => (
     {props.meta}
 
     <div className="flex min-h-full flex-col items-stretch">
-      <div className="fixed z-30 h-12 w-full items-center bg-transparent font-sans shadow-lg sm:h-14">
-        <div className="hidden">
-          <div className="">{AppConfig.title}</div>
-          <div className="text-xl">{AppConfig.description}</div>
-        </div>
-        <div className="shrink-0 py-2 px-5">
-          <ul className="flex flex-wrap text-xl">
-            <li className="mr-6">
-              <Link href="/">
-                <a className="text-color-link border-none">Home</a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <Link href="/about/">
-                <a className="text-color-link border-none">About</a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <a
-                className="text-color-link border-none"
-                href="https://github.com/ixartz/Next-js-Boilerplate"
+      <div className="navbar fixed top-0 z-50 bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                GitHub
-              </a>
-            </li>
-          </ul>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+            >
+              <li>
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/skills">
+                  <a>Skills</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <Link href="/">
+            <a className="btn btn-ghost text-xl normal-case">
+              Alejandro Oviedo
+            </a>
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge badge-xs indicator-item badge-primary"></span>
+            </div>
+          </button>
         </div>
       </div>
+
       <div className="text-xl">{props.children}</div>
-      <div className="fixed bottom-0 z-20 flex w-full justify-between border-t  border-gray-500 bg-transparent p-6 shadow-lg sm:h-16">
-        <div className="text-color font-sans text-sm ">
-          © Copyright {new Date().getFullYear()} {AppConfig.title}
+
+      <footer className="footer fixed bottom-0 z-20 items-center p-4 text-neutral-content">
+        <div className="grid-flow-col items-center">
+          <p>
+            © Copyright {new Date().getFullYear()} {AppConfig.title}
+          </p>
         </div>
-        <div className="text-color text-sm">
-          Developed by Alejandro Oviedo{' '}
-          <span role="img" aria-label="Love">
-            ♥
-          </span>{' '}
+        <div>
+          <p>Powerd by Next.js & React Three Fiber</p>
         </div>
-        <div className="text-color text-sm">
-          Powered with Next.js and Three.js
+
+        <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <div>
+            <a className="link-hover link">Contact</a>
+          </div>
+          <div>
+            <a className="link-hover link">About Me</a>
+          </div>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+            </svg>
+          </a>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+            </svg>
+          </a>
         </div>
-      </div>
+      </footer>
     </div>
   </div>
 );
